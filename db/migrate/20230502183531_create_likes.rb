869cc8ffdaa47c92286ca1677412abb06ac56454
Dennis Every <dennis@ansyra.com>
2023-05-03 08:12:30 -0400

    Change User bio field to text, change User associations to use references in migrations

--- db/migrate/20230502183531_create_likes.rb
+++ db/migrate/20230502183531_create_likes.rb
@@ -1,11 +1,10 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :author_id
+      t.references :author, null: false, foreign_key: { to_table: 'users' }
       t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :likes, :author_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_183531) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "author_id"
     t.bigint "post_id", null: false
     t.text "text"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_183531) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author_id"
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -34,11 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_183531) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
@@ -47,12 +47,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_183531) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bio"
+    t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
