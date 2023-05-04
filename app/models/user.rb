a8eaf5978293f998caecda2c8eaa8f73c5bd0583
Dennis Every <dennis@ansyra.com>
2023-05-03 22:22:02 -0400

    Add User validates name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
+  validates :name, presence: true
+
   def last_3_posts
     posts.order(created_at: :desc).limit(3)
   end
