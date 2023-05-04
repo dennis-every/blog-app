e141534348817f827422f525c599768bda04504a
Dennis Every <dennis@ansyra.com>
2023-05-03 22:23:32 -0400

    Add User spec valid posts_counter

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,9 @@ describe User, type: :model do
     user = User.new(posts_counter: -1)
     expect(user).to_not be_valid
   end
+
+  it 'is valid if posts_counter is an integer greater than or equal to zero' do
+    user = User.new(posts_counter: 0)
+    expect(user).to be_valid
+  end
 end
