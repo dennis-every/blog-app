b8a0ca562add1c80e72f38906a009efd7b7b43e7
Dennis Every <dennis@ansyra.com>
2023-05-03 22:53:42 -0400

    Add Post validations, spec updates

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,41 +5,41 @@ describe Post, type: :model do
 
   describe 'Title' do
     it 'not valid if blank' do
-      post = Post.new(title: nil, author:)
+      post = Post.new(title: nil, comments_counter: 1, likes_counter: 1, author:)
       expect(post).to_not be_valid
     end
 
     it 'valid if not blank' do
-      post = Post.new(title: 'test', author:)
+      post = Post.new(title: 'test', comments_counter: 1, likes_counter: 1, author:)
       expect(post).to be_valid
     end
 
     it 'not valid if exceeds 250 characters' do
-      post = Post.new(title: 'a' * 251, author:)
+      post = Post.new(title: 'a' * 251, comments_counter: 1, likes_counter: 1, author:)
       expect(post).to_not be_valid
     end
   end
 
   describe 'CommentsCounter' do
     it 'is not valid if it is not an integer greater than or equal to zero' do
-      post = Post.new(title: 'test', comments_counter: -1, author:)
+      post = Post.new(title: 'test', comments_counter: -1, likes_counter: 1, author:)
       expect(post).to_not be_valid
     end
 
     it 'is valid if it is an integer greater than or equal to zero' do
-      post = Post.new(title: 'test', comments_counter: 0, author:)
+      post = Post.new(title: 'test', comments_counter: 0, likes_counter: 1, author:)
       expect(post).to be_valid
     end
   end
 
   describe 'LikesCounter' do
     it 'is not valid if it is not an integer greater than or equal to zero' do
-      post = Post.new(title: 'test', likes_counter: -1, author:)
+      post = Post.new(title: 'test', comments_counter: 1, likes_counter: -1, author:)
       expect(post).to_not be_valid
     end
 
     it 'is valid if it is an integer greater than or equal to zero' do
-      post = Post.new(title: 'test', likes_counter: 0, author:)
+      post = Post.new(title: 'test', comments_counter: 1, likes_counter: 0, author:)
       expect(post).to be_valid
     end
   end
