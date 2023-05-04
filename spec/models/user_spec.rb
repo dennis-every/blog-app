788e1ea512916cb3b2877ac5f4fe65605b0e12c8
Dennis Every <dennis@ansyra.com>
2023-05-03 22:20:00 -0400

    Add User spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  it 'is not valid without a name' do
+    user = User.new(name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid if posts_counter is not an integer greater than or equal to zero' do
+    user = User.new(posts_counter: -1)
+    expect(user).to_not be_valid
+  end
+end
