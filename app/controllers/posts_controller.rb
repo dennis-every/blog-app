8b13657fc7fe7f7140776b8633c89f801d97fa9c
Dennis Every <dennis@ansyra.com>
2023-05-10 11:33:31 -0400

    Update posts#index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.last_3_posts.includes(:comments)
+  end
+
   def show; end
 end
