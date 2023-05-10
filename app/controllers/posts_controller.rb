33539ec456ae81c56329cdbc52954ea8fa3532a2
Dennis Every <dennis@ansyra.com>
2023-05-10 11:47:47 -0400

    Update posts#show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,7 @@ class PostsController < ApplicationController
     @posts = @user.last_3_posts.includes(:comments)
   end
 
-  def show; end
+  def show
+    @post = Post.includes(:comments).find(params[:id])
+  end
 end
