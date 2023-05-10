c5503e04e8b3f71305a15b032f74e8eb4998ace8
Dennis Every <dennis@ansyra.com>
2023-05-10 05:20:46 -0400

    Add request spec for users

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    it 'response status is success' do
+      get users_path
+      expect(response.status).to eq(200)
+    end
+    it 'index template was rendered' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+    it "response body includes the text 'List of users'" do
+      get users_path
+      expect(response.body).to include('List of users')
+    end
+  end
+
+  describe 'GET /show' do
+    let!(:user) { User.create(name: 'John', posts_counter: 0) }
+
+    it 'response status is success' do
+      get user_path(user.id)
+      expect(response.status).to eq(200)
+    end
+    it 'show template was rendered' do
+      get user_path(user.id)
+      expect(response).to render_template(:show)
+    end
+    it "response body includes the text 'Username'" do
+      get user_path(user.id)
+      expect(response.body).to include('Username')
+    end
+  end
+end
