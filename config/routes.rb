7d8ae6f3b689095a8f3159797e68f728e65b4517
Dennis Every <dennis@ansyra.com>
2023-05-11 17:36:22 -0400

    Add link to "Add Comment"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create], shallow: true do
+      resources :comments, only: [:new, :create], shallow: true
+    end
   end
   root 'users#index'
 end
