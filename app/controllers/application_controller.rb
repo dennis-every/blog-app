93a8baa5b6ee5659405d6b8f50c3b5374d439ea5
Dennis Every <dennis@ansyra.com>
2023-05-11 12:49:13 -0400

    Add helper_method current_user, and "Add Post" link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
   def current_user
-    User.first
+    @current_user ||= User.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new]
   end
   root 'users#index'
 end
