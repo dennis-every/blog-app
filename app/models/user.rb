9ac0fdfcc2f25d5074bdbb79ff90abd6e119e7e4
Dennis Every <dennis@ansyra.com>
2023-05-16 16:11:56 -0400

    Add system specs users#show, fix rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def last_3_posts
-    posts.order(created_at: :desc).limit(3)
+  def first_3_posts
+    posts.order(created_at: :asc).limit(3)
   end
 
   def likes?(post)
--- spec/system/users/index_spec.rb
+++ spec/system/users/index_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe 'Users index page', type: :system do
   describe 'checks the content of the users#index page' do
     before(:each) do
-      @user_one = User.create(name: 'Dennis', photo: "https://robohash.org/idquiet.png?size=300x300&set=set1", bio: 'software developer')
-      @user_two = User.create(name: 'Neville', photo: "https://robohash.org/adipiscivoluptatesea.png?size=300x300&set=set1", bio: 'data scientist')
-      @post_one = Post.create!(title: Faker::Quotes::Shakespeare.hamlet_quote, text: Faker::Quote.matz, author: @user_one)
-      @post_two = Post.create!(title: Faker::Quotes::Shakespeare.hamlet_quote, text: Faker::Quote.matz, author: @user_one)
+      @user1 = User.create(name: 'Dennis', photo: 'https://robohash.org/idquiet.png', bio: 'software developer')
+      @user2 = User.create(name: 'Neville', photo: 'https://robohash.org/adipiscivoluptatesea.png', bio: 'coder')
+      @post1 = Post.create(title: Faker::Quotes::Shakespeare.hamlet_quote, text: Faker::Quote.matz, author: @user1)
+      @post2 = Post.create(title: Faker::Quotes::Shakespeare.hamlet_quote, text: Faker::Quote.matz, author: @user1)
     end
-    
+
     it 'expects to see the username of all other users' do
       visit root_path
 
@@ -19,22 +19,22 @@ describe 'Users index page', type: :system do
     it 'expects to see the profile picture for each user' do
       visit root_path
 
-      expect(page).to have_css("img[src*='#{@user_one.photo}']")
-      expect(page).to have_css("img[src*='#{@user_two.photo}']")
+      expect(page).to have_css("img[src*='#{@user1.photo}']")
+      expect(page).to have_css("img[src*='#{@user2.photo}']")
     end
 
     it 'expects to see the number of posts each user has written' do
       visit root_path
 
-      expect(page).to have_content("Number of posts: #{@user_one.posts.size}")
-      expect(page).to have_content("Number of posts: #{@user_two.posts.size}")
+      expect(page).to have_content("Number of posts: #{@user1.posts.size}")
+      expect(page).to have_content("Number of posts: #{@user2.posts.size}")
     end
 
     it "expects on click on a user to redirect to that user's show page" do
       visit root_path
 
       click_link 'Dennis'
-      expect(page).to have_current_path(user_path(@user_one))
+      expect(page).to have_current_path(user_path(@user1))
     end
   end
 end
