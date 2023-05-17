b51c8218ad6144706ba3ecc08a286ed5f9df4e74
Dennis Every <dennis@ansyra.com>
2023-05-17 14:38:23 -0400

    Fix rubocop offenses

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_user, only: :index
   before_action :set_likes, only: :index
 
