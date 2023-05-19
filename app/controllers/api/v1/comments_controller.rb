0d24ae91147cac2bd2660d24d69fe49196afd39d
Dennis Every <dennis@ansyra.com>
2023-05-19 08:41:23 -0400

    Add post_comment.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -23,6 +23,6 @@ class Api::V1::CommentsController < Api::V1::BaseController
   private
 
   def comment_params
-    params.require(:comment).permit(:post_id, :text, :author_id)
+    params.require(:comment).permit(:post_id, :text)
   end
 end
